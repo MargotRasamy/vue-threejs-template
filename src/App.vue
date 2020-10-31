c871aeac561a33a5d262c0b7b4cd64e6c5b21e7e
margot.rasamy <rasamymargot@gmail.com>
2020-10-31 16:07:49 +0100

    Initialized threejs installation and imported robot model as example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main msg="Vue.js / Threejs template"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/scenes/Main'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
---
+++ src/components/scenes/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+    <Robot/>
+
+  </div>
+</template>
+
+<script>
+import Robot from '../models/characters/Robot'
+
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  },
+  components: {
+      Robot
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
